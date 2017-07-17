1f422aca4cfd7d4d6ca6ed78ef68a2c05c144eb4
Bryan <babuitrago655@misena.edu.co>
2017-07-17 11:25:07 -0500

    Validación añadida

--- src/java/Controllers/ControllerSession.java
+++ src/java/Controllers/ControllerSession.java
@@ -76,7 +76,13 @@ public class ControllerSession extends ControllerApp {
                 System.out.println("El usuario ingresado es: " + usuario.getPrimerNombre());
 //                FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO,"Datos de acceso correctos", null);
 //                fc.addMessage(null, m);
-                return "Usuarios/ConsultarUsuarios?faces-redrect=true";
+                if(usuario.getCodRol().getNombreRol().equals("Super Administrador")){
+                    return "Usuarios/ConsultarUsuarios?faces-redirect=true";
+                }else if(usuario.getCodRol().getNombreRol().equals("Usuario")){
+                    System.out.println("Otro rol"+ usuario.getCodRol().getNombreRol());
+                    return "UsuarioPerfil/miPerfil?faces-redirect=true";
+                }
+                
             }else{
                 FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR,"Email o clave incorrectas", "verifique los datos");
                 fc.addMessage(null, m);
