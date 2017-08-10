fba6fcc7095673ff36c8b1da7053c69bbf07ec32
Jeisson <jt2@misena.edu.co>
2017-08-09 21:04:35 -0500

    Actualizacion de Controlador Prediagnostico (metodos elimiandos)

--- src/java/Controllers/ControllerPrediagnostico.java
+++ src/java/Controllers/ControllerPrediagnostico.java
@@ -5,26 +5,16 @@
  */
 package Controllers;
 
-import Entities.Cuestionario;
+
 import Entities.Prediagnostico;
-import Entities.Respuestapreg;
-import Entities.Usuario;
-import Facade.CuestionarioFacadeLocal;
 import Facade.PrediagnosticoFacadeLocal;
-import Facade.RespuestapregFacadeLocal;
-import Facade.UsuarioFacadeLocal;
+
 import javax.inject.Named;
 import javax.enterprise.context.ConversationScoped;
-import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.inject.Inject;
+
 
 /**
  *
@@ -40,38 +30,17 @@ public class ControllerPrediagnostico extends ControllerApp {
     public ControllerPrediagnostico() {
     }
 
-    @Inject
-    ControllerUsuario controllerUsuario;
-    ControllerCuestionario controllerCuestionario;
-    ControllerRespuestaPreg controllerRespuestaPreg;
-
+     
     @EJB
-    PrediagnosticoFacadeLocal prediagnosticoFacade;
-    Prediagnostico prediagnostico;
-    List<Prediagnostico> listaPrediagnistico;
+    private PrediagnosticoFacadeLocal prediagnosticoFacade;
+    private Prediagnostico prediagnostico;
+    private List<Prediagnostico> listaPrediagnistico;
     private Prediagnostico prediagnosticoSeleccionado;
-    
-    @EJB
-    CuestionarioFacadeLocal cuestionarioFacade;
-    Cuestionario cuestionario;
-    List<Cuestionario> listaCuestionario;
 
-    @EJB
-    RespuestapregFacadeLocal respuestapregFacade;
-    Respuestapreg respuestapreg;
-    List<Respuestapreg> listaRespuestapreg;
-
-    @EJB
-    UsuarioFacadeLocal usuarioFacade; //Facade
-    Usuario usuario; //Entidades
-    List<Usuario> listaUsuarios;
 
     @PostConstruct
     public void init() {
         this.prediagnostico = new Prediagnostico();
-        this.cuestionario = new Cuestionario();
-        this.respuestapreg = new Respuestapreg();
-        this.usuario = new Usuario();
         this.listaPrediagnistico = prediagnosticoFacade.findAll();
     }
 
@@ -92,33 +61,9 @@ public class ControllerPrediagnostico extends ControllerApp {
         finalizarConversacion();
         return "ConsultarPrediagnostico?faces-redirect=true";
     }
+  
 
-    public String seleccionarCrear() {
-        System.out.println("HOLA");
-        iniciarConversacion();
-        return "";
-    }
-
-    public String crearPrediagnostico() throws ParseException {
-        System.out.println("Estamos creando un hsitorial clinico");
-        Calendar datosFecha = new GregorianCalendar();
-        int anio = datosFecha.get(Calendar.YEAR);
-        int mes = datosFecha.get(Calendar.MONTH);
-        int dia = datosFecha.get(Calendar.DAY_OF_MONTH);
-        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
-        String strFecha = anio + "-" + mes + "-" + dia;
-        Date fechaDate = null;
-        fechaDate = formato.parse(strFecha);
-//            this.historialClinico.setCodUsuario(usuarioFacade.find(usuario.getIdUsuario()));
-        prediagnostico.setFecha(fechaDate);
-        prediagnostico.setCodUsuario(controllerUsuario.getUsuario());
-        prediagnostico.setCodCuestionario(controllerCuestionario.getListaCuestionario().get(1));
-        prediagnostico.setCodRespuestaPreg(controllerRespuestaPreg.getRespuestapreg());
-        this.prediagnosticoFacade.create(prediagnostico);
-        return "ConsultarPrediagnostico?faces-redirect=true";
-    }  
-    
-    public String cancelarActualizar(){
+    public String cancelarActualizar() {
         finalizarConversacion();
         return "ConsultarPrediagnostico?faces-redirect=true";
     }
@@ -147,78 +92,6 @@ public class ControllerPrediagnostico extends ControllerApp {
         this.listaPrediagnistico = listaPrediagnistico;
     }
 
-    public CuestionarioFacadeLocal getCuestionarioFacade() {
-        return cuestionarioFacade;
-    }
-
-    public void setCuestionarioFacade(CuestionarioFacadeLocal cuestionarioFacade) {
-        this.cuestionarioFacade = cuestionarioFacade;
-    }
-
-    public Cuestionario getCuestionario() {
-        return cuestionario;
-    }
-
-    public void setCuestionario(Cuestionario cuestionario) {
-        this.cuestionario = cuestionario;
-    }
-
-    public List<Cuestionario> getListaCuestionario() {
-        return listaCuestionario;
-    }
-
-    public void setListaCuestionario(List<Cuestionario> listaCuestionario) {
-        this.listaCuestionario = listaCuestionario;
-    }
-
-    public RespuestapregFacadeLocal getRespuestapregFacade() {
-        return respuestapregFacade;
-    }
-
-    public void setRespuestapregFacade(RespuestapregFacadeLocal respuestapregFacade) {
-        this.respuestapregFacade = respuestapregFacade;
-    }
-
-    public Respuestapreg getRespuestapreg() {
-        return respuestapreg;
-    }
-
-    public void setRespuestapreg(Respuestapreg respuestapreg) {
-        this.respuestapreg = respuestapreg;
-    }
-
-    public List<Respuestapreg> getListaRespuestapreg() {
-        return listaRespuestapreg;
-    }
-
-    public void setListaRespuestapreg(List<Respuestapreg> listaRespuestapreg) {
-        this.listaRespuestapreg = listaRespuestapreg;
-    }
-
-    public UsuarioFacadeLocal getUsuarioFacade() {
-        return usuarioFacade;
-    }
-
-    public void setUsuarioFacade(UsuarioFacadeLocal usuarioFacade) {
-        this.usuarioFacade = usuarioFacade;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public List<Usuario> getListaUsuarios() {
-        return listaUsuarios;
-    }
-
-    public void setListaUsuarios(List<Usuario> listaUsuarios) {
-        this.listaUsuarios = listaUsuarios;
-    }
-
     public Prediagnostico getPrediagnosticoSeleccionado() {
         return prediagnosticoSeleccionado;
     }
@@ -228,4 +101,5 @@ public class ControllerPrediagnostico extends ControllerApp {
     }
 
     
+
 }
