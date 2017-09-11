021d6910715d34f38a9143980ef2e6f8c685634c
Bryan <babuitrago655@misena.edu.co>
2017-09-10 20:58:15 -0500

    Creacion de query para buscar fisioterapueta por usuario

--- src/java/Facade/FisioterapeutaFacade.java
+++ src/java/Facade/FisioterapeutaFacade.java
@@ -6,9 +6,12 @@
 package Facade;
 
 import Entities.Fisioterapeuta;
+import Entities.Usuario;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 
 /**
  *
@@ -27,6 +30,20 @@ public class FisioterapeutaFacade extends AbstractFacade<Fisioterapeuta> impleme
 
     public FisioterapeutaFacade() {
         super(Fisioterapeuta.class);
+    }
+
+    @Override
+    public Fisioterapeuta buscarPorCodUsuario(Usuario codUsuario) {
+        Fisioterapeuta fisioterapeuta = null; 
+        try {
+            TypedQuery<Fisioterapeuta> q = getEntityManager().createNamedQuery("Fisioterapeuta.buscarPorCodUsuario", Fisioterapeuta.class);
+            q.setParameter("codUsuario", codUsuario);
+            fisioterapeuta = q.getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return fisioterapeuta;
     }
     
 }
