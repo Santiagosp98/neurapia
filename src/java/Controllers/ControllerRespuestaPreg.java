2869f4c6c96876f0d8ab5add9c9883b8414457be
Santiago Samper <jokerwrd@gmail.com>
2017-09-11 13:00:44 -0500

    Validaciones añadidas al método crearRespuestaPreg()

--- src/java/Controllers/ControllerRespuestaPreg.java
+++ src/java/Controllers/ControllerRespuestaPreg.java
@@ -10,17 +10,20 @@ import Entities.Respuestapreg;
 import Facade.CuestionarioFacadeLocal;
 import Facade.PrediagnosticoFacadeLocal;
 import Facade.RespuestapregFacadeLocal;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.enterprise.context.ConversationScoped;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Inject;
 
 /**
- *
  * @author Jeisson Diaz
  */
 @Named(value = "controllerRespuestaPreg")
@@ -29,19 +32,19 @@ public class ControllerRespuestaPreg extends ControllerApp {
 
     @Inject
     private ControllerSession cs;
-    
-    @EJB 
+
+    @EJB
     private PrediagnosticoFacadeLocal pfl;
 
     @EJB
     private CuestionarioFacadeLocal cfl;
-    
+
     @EJB
     private RespuestapregFacadeLocal respuestapregFacade;
     private Respuestapreg respuestapreg;
     private List<Respuestapreg> listaRespuestapreg;
     private Respuestapreg respuestapregSeleccionado;
-    
+
     /**
      * Creates a new instance of ControllerRespuestaPreg
      */
@@ -60,9 +63,77 @@ public class ControllerRespuestaPreg extends ControllerApp {
     }
 
     public String crearRespuestapreg() {
-        
-        System.out.println("iniciando crear respuestas");
-        this.respuestapregFacade.create(respuestapreg); 
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+
+        Pattern regexp;
+        regexp = Pattern.compile("^[a-zA-ZáéíóúÁÉÍÓÚ]+$");
+        int invalid = 0;
+
+        if (!regexp.matcher(respuestapreg.getResp1()).matches()) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_WARN, "No se permiten carácteres que no pertenezcan al alfabeto.", "");
+            facesContext.addMessage("pregunta1", m);
+            invalid++;
+        }
+
+        if (!regexp.matcher(respuestapreg.getResp2()).matches()) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_WARN, "No se permiten carácteres que no pertenezcan al alfabeto.", "");
+            facesContext.addMessage("pregunta2", m);
+            invalid++;
+        }
+
+        if (!regexp.matcher(respuestapreg.getResp3()).matches()) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_WARN, "No se permiten carácteres que no pertenezcan al alfabeto.", "");
+            facesContext.addMessage("pregunta3", m);
+            invalid++;
+        }
+
+        if (!regexp.matcher(respuestapreg.getResp4()).matches()) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_WARN, "No se permiten carácteres que no pertenezcan al alfabeto.", "");
+            facesContext.addMessage("pregunta4", m);
+            invalid++;
+        }
+
+        if (!regexp.matcher(respuestapreg.getResp5()).matches()) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_WARN, "No se permiten carácteres que no pertenezcan al alfabeto.", "");
+            facesContext.addMessage("pregunta5", m);
+            invalid++;
+        }
+
+        if (!regexp.matcher(respuestapreg.getResp6()).matches()) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_WARN, "No se permiten carácteres que no pertenezcan al alfabeto.", "");
+            facesContext.addMessage("pregunta6", m);
+            invalid++;
+        }
+
+        if (!regexp.matcher(respuestapreg.getResp7()).matches()) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_WARN, "No se permiten carácteres que no pertenezcan al alfabeto.", "");
+            facesContext.addMessage("pregunta7", m);
+            invalid++;
+        }
+
+        if (!regexp.matcher(respuestapreg.getResp8()).matches()) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_WARN, "No se permiten carácteres que no pertenezcan al alfabeto.", "");
+            facesContext.addMessage("pregunta8", m);
+            invalid++;
+        }
+
+        if (!regexp.matcher(respuestapreg.getResp9()).matches()) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_WARN, "No se permiten carácteres que no pertenezcan al alfabeto.", "");
+            facesContext.addMessage("pregunta9", m);
+            invalid++;
+        }
+
+        if (!regexp.matcher(respuestapreg.getResp10()).matches()) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_WARN, "No se permiten carácteres que no pertenezcan al alfabeto.", "");
+            facesContext.addMessage("pregunta10", m);
+            invalid++;
+        }
+
+        if (invalid > 0) {
+            return "";
+        }
+
+        this.respuestapregFacade.create(respuestapreg);
         Prediagnostico pre = new Prediagnostico();
         pre.setCodUsuario(cs.getUsuario());
         pre.setCodRespuestaPreg(respuestapreg);
@@ -72,9 +143,9 @@ public class ControllerRespuestaPreg extends ControllerApp {
         pfl.create(pre);
         return "ConsultarPrediagnostico?faces-redirect=true";
     }
-    
-    public String aceptar(){
-        
+
+    public String aceptar() {
+
         return "ConsultarPrediagnostico?faces-redirect=true";
     }
 
