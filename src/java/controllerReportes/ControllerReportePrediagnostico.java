61b747be4fbdf8e3df75b1d0b4e83d841390a864
jt0708 <jt2@misena.edu.co>
2017-10-15 09:57:40 -0500

    Metodos para generar grafico

--- src/java/controllerReportes/ControllerReportePrediagnostico.java
+++ src/java/controllerReportes/ControllerReportePrediagnostico.java
@@ -21,6 +21,9 @@ import javax.persistence.PersistenceContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -79,6 +82,32 @@ public class ControllerReportePrediagnostico   {
         res.addHeader("Content-disposition", "attachment; filename=\"ReportePrediagnosticoPendiente.pdf\"");
         out = res.getOutputStream();
         JasperExportManager.exportReportToPdfStream(jp, out);
+        fc.responseComplete();
+    }
+
+    private void prepareGraphics() throws JRException, ClassNotFoundException, SQLException, IOException{
+        Connection conn;
+        Class.forName("com.mysql.jdbc.Driver");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistemaneurapia?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","jair","3208743082");
+        Map<String, Object> params = new HashMap<>();
+//        params.put("NombreSesssion", cS.getUsuario().getFullNameUsuario());
+        JRBeanCollectionDataSource bcds = new JRBeanCollectionDataSource(listaPrediagnosticoResultado);
+        String reportPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/")+ "/WEB-INF/reportes/prediagnostico/graficos.jasper";
+        jP = JasperFillManager.fillReport(reportPath, params, conn);
+
+    }
+    public void exportGraphicsPDF() throws IOException, JRException, ClassNotFoundException, SQLException{
+//        System.out.println(listaReporte.toString());
+        prepareGraphics();
+        ServletOutputStream out = null;
+        String contentType =  "application/pdf";
+        FacesContext fc = FacesContext.getCurrentInstance();
+        ExternalContext ec = fc.getExternalContext();
+        HttpServletResponse res = (HttpServletResponse) ec.getResponse();
+        res.setContentType(contentType);
+        res.addHeader("Content-disposition", "attachment; filename=\"GraficoEstadoPrediagnostico.pdf\"");
+        out = res.getOutputStream();
+        JasperExportManager.exportReportToPdfStream(jP, out);
         fc.responseComplete();
     }
 }
