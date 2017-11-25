7724d036afa05c7d2f21a492502c274554dd898a
Santiago Samper <jokerwrd@gmail.com>
2017-11-25 14:53:46 -0500

    Método listadoTipoActividades() modificado para que retorne la lista de actividades según las actividades en proceso

--- src/java/Controllers/ControllerTipoActividad.java
+++ src/java/Controllers/ControllerTipoActividad.java
@@ -10,18 +10,23 @@ import Entities.Respuestaactividad;
 import Entities.Tipoactividad;
 import Facade.ActividadFacadeLocal;
 import Facade.TipoactividadFacadeLocal;
+import org.jboss.weld.context.http.Http;
+
+import javax.faces.context.FacesContext;
+import javax.faces.convert.FacesConverter;
 import javax.inject.Named;
 import javax.enterprise.context.ConversationScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 /**
- *
  * @author jair3
  */
 @Named(value = "controllerTipoActividad")
@@ -31,31 +36,33 @@ public class ControllerTipoActividad extends ControllerApp {
     /**
      * Creates a new instance of ControllerTipoActividad
      */
-    
-    @Inject ControllerRespuestaActividad resAct;
-    
+
+    @Inject
+    private ControllerRespuestaActividad resAct;
+    private Respuestaactividad respuestaactividad;
+
     @EJB
     private TipoactividadFacadeLocal tipoActividadFacade;
     private Tipoactividad tipoActividad;
     private List<Tipoactividad> listaTipoActividades;
-    
-    
+
+
     @EJB
     private ActividadFacadeLocal actividadFacade;
     private Actividad actividad;
     private List<Actividad> listaActividad;
-    
-    
+
+
     public ControllerTipoActividad() {
     }
-    
+
     @PostConstruct
-    public void init(){
-       this.tipoActividad = new Tipoactividad();
-       listaTipoActividades = tipoActividadFacade.findAll();
-       
-       this.actividad = new Actividad();
-       listaActividad = actividadFacade.findAll();
+    public void init() {
+        this.tipoActividad = new Tipoactividad();
+        listaTipoActividades = tipoActividadFacade.findAll();
+
+        this.actividad = new Actividad();
+        listaActividad = actividadFacade.findAll();
     }
 
     public TipoactividadFacadeLocal getTipoActividadFacade() {
@@ -81,9 +88,32 @@ public class ControllerTipoActividad extends ControllerApp {
     public void setListaTipoActividades(List listaTipoActividades) {
         this.listaTipoActividades = listaTipoActividades;
     }
-    
-    public List<Tipoactividad> listadoTipoActividades(){
+
+    public Respuestaactividad getRespuestaactividad() {
+        return respuestaactividad;
+    }
+
+    public void setRespuestaactividad(Respuestaactividad respuestaactividad) {
+        this.respuestaactividad = respuestaactividad;
+    }
+
+    public List<Tipoactividad> listadoTipoActividades() {
         this.listaTipoActividades = tipoActividadFacade.findAll();
+        Iterator<Tipoactividad> tipoactividadIterator = listaTipoActividades.iterator();
+        while (tipoactividadIterator.hasNext()) {
+            Tipoactividad tipoactividad = tipoactividadIterator.next();
+            for (Respuestaactividad respuestaactividad:
+                 resAct.listarRespuestasActividad()) {
+                if (Objects.equals(tipoactividad.getIdTipoActividad(), respuestaactividad.getCodTipoActividad().getIdTipoActividad()) && Objects.equals(respuestaactividad.getEstado(), "En proceso")) {
+                    tipoactividadIterator.remove();
+                }
+            }
+        }
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String uri = request.getRequestURI();
+        if (uri.endsWith("editaractividad.xhtml")) {
+            listaTipoActividades.add(respuestaactividad.getCodTipoActividad());
+        }
         return listaTipoActividades;
     }
 
@@ -110,8 +140,8 @@ public class ControllerTipoActividad extends ControllerApp {
     public void setListaactividad(List listaactividad) {
         this.listaActividad = listaactividad;
     }
-    
-    public void crearTipoActividad(){
+
+    public void crearTipoActividad() {
         try {
             System.out.println("Estamos creando un tipo de actividad");
             this.tipoActividadFacade.create(tipoActividad);
@@ -120,8 +150,8 @@ public class ControllerTipoActividad extends ControllerApp {
             System.out.println(e);
         }
     }
-    
-    public void editarTipoActividad(){
+
+    public void editarTipoActividad() {
         try {
             if (tipoActividadFacade != null) {
                 System.out.println("Estamos editando los tipos de actividad");
@@ -130,15 +160,17 @@ public class ControllerTipoActividad extends ControllerApp {
         } catch (Exception e) {
             System.out.println(e);
         }
-        
+
     }
 
-    public List seleccionTiposdeActividad(){
+    public List seleccionTiposdeActividad() {
         ArrayList lista = new ArrayList();
         lista.add("Medios Físicos");
         lista.add("Electroterapia");
         lista.add("Ejercicios prueba");
         return lista;
     }
+
+
 
 }
